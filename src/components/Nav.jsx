a4dac0c4e9ce0ab1e8602fdf8a08a1f54ee9b7a0
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2023-12-02 11:30:22 +0530

    git remote add origin https://github.com/Rajeshkumarachari/nike.git

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,17 +3,17 @@ import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
 const Nav = () => {
   return (
-    <header className=" padding-x py-8 absolute  w-full">
+    <header className=" padding-x py-8 absolute  w-full z-20">
       <nav className=" flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
         <ul className=" flex-1 flex justify-center text-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
-            <li key={item.label}>
+            <li key={item.label} className=" cursor-pointer">
               <a
                 href={item.href}
-                className=" font-montserrat leading-normal text-lg text-slate-gray"
+                className=" font-montserrat cursor-pointer leading-normal text-lg text-slate-gray"
               >
                 {item.label}
               </a>

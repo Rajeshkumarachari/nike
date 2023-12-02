7507c4f986463b715ec053f61dd2b9236f98defe
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2023-12-02 18:01:05 +0530

    offer done

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,9 +1,9 @@
 import { star } from "../assets/icons";
 const PopularProductCard = ({ imgURL, name, price }) => {
   return (
-    <div className=" flex flex-1 flex-col w-full max-sm:w-full ">
-      <img src={imgURL} alt={name} className=" w-[280px] h-[280px] " />
-      <div className=" mt-8 flex justify-start gap-2.5 ">
+    <div className=" flex flex-1 flex-col w-full max-sm:w-full shadow-md rounded-sm cursor-pointer ">
+      <img src={imgURL} alt={name} className=" w-[280px] h-[280px] pl-11  " />
+      <div className=" mt-8 flex justify-start gap-1 sm:gap-2.5 ">
         <img src={star} alt="rating" width={24} height={24} />
         <p className=" font-montserrat text-xl leading-normal text-slate-gray">
           (4.7)
@@ -12,8 +12,8 @@ const PopularProductCard = ({ imgURL, name, price }) => {
       <h3 className=" mt-2 text-2xl leading-normal font-semibold font-palanquin">
         {name}
       </h3>
-      <p className=" mt-2 font-semibold font-montserrat text-coral-red text text-2xl leading-normal">
-        {price}{" "}
+      <p className=" mb-3 mt-2 font-semibold font-montserrat text-coral-red text text-2xl leading-normal">
+        {price}
       </p>
     </div>
   );
